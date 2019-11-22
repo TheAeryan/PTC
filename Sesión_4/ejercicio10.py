70fd16bf342d822dbd50c803c60d22a367633cdc
TheAeryan <carlosmarista@gmail.com>
2019-11-22 13:35:50 +0100

    Finished ejercicio10 sesión 4

--- Sesión_4/ejercicio10.py
+++ Sesión_4/ejercicio10.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 """
+@author: Carlos Núñez Molina
+"""
+
+
+"""
 10. Leer una frase de teclado e implementar una función que devuelva una lista de pares en la que
 debe aparecer cada letra junto a su frecuencia de aparición. Los espacios no se deben tener en
 cuenta. Dicha lista debe estar ordenada atendiendo al orden ascendente de las letras. Ejemplo: ante
 la entrada “programa” debe dar como salida [('a', 2), ('g', 1), ('m',1), ('o', 1), ('p',1), ('r',2)].
 """
-
-# HACER UNA VERSIÓN CON LAS FUNCIONES DE LIST Y OTRA SIN USARLA
 
 # Función que no usa los métodos de string
 def devolverFrecuenciasLetras1(frase):
@@ -37,7 +40,18 @@ def devolverFrecuenciasLetras1(frase):
    
 # Función que usa los métodos de string
 def devolverFrecuenciasLetras2(frase):
-     pass
+     # Creo un set con los caracteres de frase, menos el espacio
+     set_carac = set(frase)
+     set_carac.discard(' ') # Elimina el espacio en blanco, en caso de existir en el set
+     
+     # Lo paso a una lista y ordeno los caracteres por orden alfabético
+     list_carac_ord = sorted(list(set_carac))
+     
+     # Uso la función count para obtener el número de ocurrencias
+     # de cada caracter en la frase
+     list_frec_ord = [(carac, frase.count(carac)) for carac in list_carac_ord]
+     
+     return list_frec_ord
         
 if __name__ == '__main__':
     frase = input('Introduzca una frase: ')
@@ -46,7 +60,6 @@ if __name__ == '__main__':
     lista_frecuencias1 = devolverFrecuenciasLetras1(frase)
     lista_frecuencias2 = devolverFrecuenciasLetras2(frase)
     
-    if lista_frecuencias1 == lista_frecuencias2:
-        print("\nLa lista de frecuencias es:", lista_frecuencias1)
-    else:
-        print("ERROR -> ambas funciones no devuelven el mismo resultado!")
+    print("<Lista de frecuencias>")
+    print("\nSin los métodos de string:", lista_frecuencias1)
+    print("\nCon los métodos de string:", lista_frecuencias2)
